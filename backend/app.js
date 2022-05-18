66bcdd08b2df39d69627082ed75a58a49452c6b0
Risabh <khandelwalrish333@gmail.com>
2022-05-19 03:58:35 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const errorMiddleware = require("./midlleware/error");
 const path = require("path");
 
 const corsConfig = {
-  credentials: include,
+  credentials: false,
   origin:"https://subtle-kringle-c7fed5.netlify.app",
 };
 __dirname = path.resolve()
@@ -37,7 +37,7 @@ app.use("/api/v1",movie);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
   res.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
   res.set('Access-Control-Allow-Headers', 'Content-Type')
