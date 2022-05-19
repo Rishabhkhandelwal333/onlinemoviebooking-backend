4c199cbbaf7d83e5edce7e13fafddfe9d1085425
Risabh <khandelwalrish333@gmail.com>
2022-05-20 00:14:30 +0530

    first commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,8 +5,8 @@ const sendToken = (user,statuscode,res)=>{
         expires:new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 1000
         ),
-        secure: process.env.ENVIRONMENT === 'LIVE',
-        sameSite: process.env.ENVIRONMENT === 'LIVE' ? 'none' : 'lax',
+        secure: true,
+        sameSite: 'none',
         httpOnly: true,
     };
 
